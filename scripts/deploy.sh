29474c54ccd500bb0218c875da329424f9dfc70d
Mihir Joshi <mihir67mj@gmail.com>
2020-01-27 19:45:32 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 gem install --user travis
-travis login --pro
-travis encrypt --pro HEROKU_API_KEY=63d7fc3b-4bc5-4ee9-bf3a-d2d6dee988b6 -r mformihir/agile-sands-95544
+travis env set HEROKU_API_KEY 63d7fc3b-4bc5-4ee9-bf3a-d2d6dee988b6 --private -r mformihir/agile-sands-95544
 git remote add heroku git@heroku.com:agile-sands-95544.git
 mvn clean heroku:deploy-war
